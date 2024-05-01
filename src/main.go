827696768e4032ed1c06b80692b8420f5fb7f53c
Aguiar575 <aguiarus99arthurus@gmail.com>
2024-05-01 19:49:36 -0300

    adding fractal animation

--- src/main.go
+++ src/main.go
@@ -1,46 +1,69 @@
 package main
 
 import (
-    "fmt"
-    "strings"
-    "unicode/utf8"
+	"fmt"
+	"strings"
+	"time"
+	"unicode/utf8"
 )
 
-var order = 4 
-var grain = "*" 
+var grain = "*"
 
 func main() {
-    // Initialize the pyramid with one element containing the base unit surrounded by spaces
-    t := []string{grain + strings.Repeat(" ", utf8.RuneCountInString(grain))}
-    fmt.Println("Initial t:", t)
-    
-    for ; order > 0; order-- {
-        fmt.Println("Current order:", order)
-        
-        // Calculate the number of spaces needed to center the base unit in the current level
-        sp := strings.Repeat(" ", utf8.RuneCountInString(t[0])/2)
-        fmt.Println("Current sp:", sp)
-        
-        top := make([]string, len(t))
-        
-        for i, s := range t {
-            fmt.Printf("Current i:%d, s:%s\n", i, s)
-            
-            // Add spaces on both sides of the base unit to center it in the current level
-            top[i] = sp + s + sp
-            fmt.Println("Current top:", top)
-            
-            t[i] += s
-            fmt.Println("Updated t[i]:", t[i])
-        }
-        
-        t = append(top, t...)
-        fmt.Println("Updated t:", t)
-    }
-    
-    fmt.Println("Final t:")
-    for _, r := range t {
-        fmt.Println(r)
-    }
+	for {
+		clearScreen()
+		order := 5
+
+		// Initialize the pyramid with one element containing the base unit surrounded by spaces
+		fractalBody := []string{grain + strings.Repeat(" ", utf8.RuneCountInString(grain))}
+
+		for order > 0 {
+			// Calculate the number of spaces needed to center the base unit in the current level
+			sp := strings.Repeat(" ", utf8.RuneCountInString(fractalBody[0])/2)
+
+			top := cratePiramidSlice(fractalBody, sp)
+			fractalBody = append(top, fractalBody...)
+
+			printTheActualStateOfPiramid(fractalBody)
+
+			order--
+		}
+
+		time.Sleep(500 * time.Millisecond)
+
+		clearScreen()
+	}
 }
 
+func cratePiramidSlice(t []string, sp string) []string {
+	top := make([]string, len(t))
+
+	setSpacesAroundTheGrain(t, top, sp)
+	return top
+}
+
+func setSpacesAroundTheGrain(t []string, top []string, sp string) {
+	for i, s := range t {
+		top[i] = sp + s + sp
+		t[i] += s
+	}
+}
+
+func printTheActualStateOfPiramid(t []string) {
+	// This helps to create the animation effect
+	moveCursorToTopLeft()
+
+	for _, r := range t {
+		fmt.Println(r)
+	}
+
+	time.Sleep(500 * time.Millisecond)
+}
+
+func clearScreen() {
+	fmt.Print("\033[H\033[2J")
+}
+
+func moveCursorToTopLeft() {
+	fmt.Print("\033[H")
+}
